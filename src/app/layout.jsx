841a5bd853f3de26927861ee91bf0dac9518c5aa
Ashures <ashuresyt@gmail.com>
2023-10-29 16:12:13 +1300

    Removed default code from create-next-app

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'Video Player | Ashures',
+  description: 'Made by Ashures',
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Video from "@/components/Video";
+
+const Page = () => {
+  return (
+    <>
+      <h1>Video Player</h1>
+      <Video />
+    </>
+  );
+}
+
+export default Page;
