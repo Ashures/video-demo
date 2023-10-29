411dca4c3456b5a6467407c292fd9e399dc576da
Ashures <ashuresyt@gmail.com>
2023-10-29 16:16:09 +1300

    Added skeleton html to Video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,14 @@
 const Video = () => {
 	return (
 		<div className="video-container">
-
+			<div className="video-overlay">
+				<div className="video-content">
+					<video>
+						<source src="" type="" />
+					</video>
+					hi
+				</div>
+			</div>
 		</div>
 	);
 }
